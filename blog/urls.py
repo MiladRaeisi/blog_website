3757b5b3256638737bece2415a6fb314b4164a85
MiladRaeisi <mldraeisi68@gmail.com>
2024-09-02 20:28:45 +0330

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list_view, name='posts_list'),
-    path('', views.post_detail_view, name='post_detail'),
+    path('<int:pk>/', views.post_detail_view, name='post_detail'),
 ]
