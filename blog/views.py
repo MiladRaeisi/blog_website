488cdb3aea50a47ddd56192d63dada4482471860
MiladRaeisi <mldraeisi68@gmail.com>
2024-09-02 13:14:29 +0330

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
+from blog.models import Post
 from django.shortcuts import render
 
-# Create your views here.
+
+def post_list_view(request):
+    posts_list = Post.objects.all()
+    return render(request, 'blog/posts_list.html', {'posts_list': posts_list})
